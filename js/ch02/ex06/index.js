14cf510a1c4bc1f9cba5071f7d1033f753e897b9
hhoo818 <133296715+hhoo818@users.noreply.github.com>
2024-01-12 11:28:35 +0900

    ll

--- js/ch02/ex06/index.js
+++ js/ch02/ex06/index.js
@@ -7,5 +7,5 @@ FiZZBuzz問題とは
 – 間違えた人から脱落
 細かな違いはあるでしょうが大体上記のようなルールのゲームです。
 */
-
+// prettier-ignore
 export function fizzbuzz() {let result = "";for (var i = 1; i <= 100; i++) {result += i % 3 === 0 && i % 5 === 0 ? "FizzBuzz": i % 3 === 0? "Fizz": i % 5 === 0? "Buzz": i;result += "\n"; } return result;}
