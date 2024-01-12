54187afade0f8dd0151a26cd7c8ac9b13aff3abd
hhoo818 <133296715+hhoo818@users.noreply.github.com>
2024-01-12 10:35:50 +0900

    nj

--- ts/ch01/ex07/index.ts
+++ ts/ch01/ex07/index.ts
@@ -1,29 +1,3 @@
-export function abs(x: number) {
-  return Math.abs(x);
-}
-
-export function sum(numbers: number[]) {
-  return numbers.reduce((acc, current) => acc + current, 0);
-}
-
-export function factorial(n: number): number {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-}
-
-export function fib(n: number, memo: number[] = []): number {
-  if (n <= 1) {
-    return n;
-  }
-  if (memo[n] !== undefined) {
-    return memo[n];
-  }
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
-  return memo[n];
-}
 export class Point {
   x: number;
   y: number;
