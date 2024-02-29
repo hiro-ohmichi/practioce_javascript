3b6108b01d5ec50d451ba1d2222b814e53963717
hhoo818 <133296715+hhoo818@users.noreply.github.com>
2024-02-29 09:19:52 +0900

    km

--- exercises/ch06/ex06/index.test.ts
+++ exercises/ch06/ex06/index.test.ts
@@ -1,38 +1,54 @@
 import { getAllPropertyNames } from "./index.ts";
 
-class Parent {
-  parentProp1: string;
-  constructor(prop1: string) {
-    this.parentProp1 = prop1;
-  }
-}
-
-class Child extends Parent {
-  childProp1: number;
-  constructor(prop1: string, prop2: number) {
-    super(prop1);
-    this.childProp1 = prop2;
-  }
-}
-
-const symbolProp = Symbol("symbolProp");
-
-const testObj = {
-  a: 1,
-  b: 2,
-  [symbolProp]: "symbolPropValue",
-};
-
-const childInstance = new Child("Hello", 42);
-
 describe("test getAllPropertyNames", () => {
-  test("test include symbol object", () => {
-    expect(getAllPropertyNames(testObj)).toEqual(["a", "b", "symbolPropValue"]);
+  test("test obj", () => {
+    const testParentObj = {
+      abc: 123,
+      aaa: 456,
+    };
+
+    const testChildObject = Object.create(testParentObj);
+    testChildObject.a = 1;
+    testChildObject.b = 1;
+    const symbolProp1 = Symbol("symbolProp");
+    const symbolProp2 = Symbol("symbolProp");
+    testChildObject[symbolProp1] = "symbolPropValue";
+    testChildObject[symbolProp2] = "symbolPropValue";
+
+    expect(getAllPropertyNames(testChildObject)).toEqual([
+      "a",
+      "b",
+      "0", //symbolPropになぜかならない
+      "1", //symbolPropになぜかならない
+      "length",
+      "abc",
+      "aaa",
+    ]);
   });
-  test("test include child object", () => {
-    expect(getAllPropertyNames(childInstance)).toBe([
-      "childProp1",
+  test("test class", () => {
+    class Parent {
+      parentProp1: string;
+      constructor(prop1: string) {
+        this.parentProp1 = prop1;
+      }
+    }
+
+    class Child extends Parent {
+      childProp1: number;
+      constructor(prop1: string, prop2: number) {
+        super(prop1);
+        this.childProp1 = prop2;
+      }
+    }
+
+    const childInstance = new Child("Hello", 42);
+
+    expect(getAllPropertyNames(childInstance)).toEqual([
       "parentProp1",
+      "childProp1",
+      "length",
     ]);
   });
 });
+
+//オブジェクトは子→親の順番で検索されるが、クラスは親→子で検索される
