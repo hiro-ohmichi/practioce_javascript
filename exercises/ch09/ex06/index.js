a7d2d22ac3c18510d1a9dfea59fc1c47fd9a1d81
hhoo818 <133296715+hhoo818@users.noreply.github.com>
2024-04-15 17:06:27 +0900

    save

---
+++ exercises/ch09/ex06/index.js
@@ -0,0 +1,24 @@
+export class TypedMap {
+  constructor(keyType, valueType, entries) {
+    if (entries) {
+      for (let { k, v } of entries) {
+        if (typeof k !== keyType || typeof v !== valueType) {
+          throw new TypeError(`Wrong type for entry [${k},${v}]`);
+        }
+      }
+    }
+    this.keyType = keyType;
+    this.valueType = valueType;
+    this.map = new Map(entries);
+  }
+
+  set(key, value) {
+    if (this.keyType && typeof key !== this.keyType) {
+      throw new TypeError(`${key} is not of type ${this.keyType}`);
+    }
+    if (this.valueType && typeof value !== this.valueType) {
+      throw new TypeError(`${value} is not of type ${this.valueType}`);
+    }
+    return this.map(key, value);
+  }
+}
